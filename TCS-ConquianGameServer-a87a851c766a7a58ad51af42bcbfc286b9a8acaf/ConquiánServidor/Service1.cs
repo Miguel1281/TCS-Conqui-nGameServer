2a3cb22a6396fdeb9c842af81797c60e712a09b9
Miguel <miguelmendez765@gmail.com>
2025-10-09 09:56:29 -0600

    Servidor

---
+++ TCS-ConquianGameServer-a87a851c766a7a58ad51af42bcbfc286b9a8acaf/ConquiánServidor/Service1.cs
@@ -0,0 +1,50 @@
+﻿using ConquiánServidor.ConquiánDB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ConquiánServidor
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "Service1" en el código y en el archivo de configuración a la vez.
+    public class Service1 : IService1
+    {
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+
+        public bool RegisterPlayer(Player newPlayer)
+        {
+            try
+            {
+                using (var context = new ConquiánDBEntities())
+                {
+                    context.Player.Add(newPlayer);
+                    context.SaveChanges();
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+    }
+}

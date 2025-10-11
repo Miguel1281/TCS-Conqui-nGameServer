64f4e5131aeb1546cafd4c65f83b95c947aca426
Miguel <miguelmendez765@gmail.com>
2025-10-10 18:57:05 -0600

    Implementacion parcial del servicio de Login

---
+++ TCS-ConquianGameServer/ConquiánServidor/Contracts/ILogin.cs
@@ -0,0 +1,17 @@
+﻿using ConquiánServidor.ConquiánDB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConquiánServidor.Contracts
+{
+    [ServiceContract]
+    internal interface ILogin
+    {
+        [OperationContract]
+        bool SignIn(string mail, string password);
+    }
+}
---
+++ TCS-ConquianGameServer/ConquiánServidor/Services/Login.cs
@@ -0,0 +1,30 @@
+﻿using ConquiánServidor.ConquiánDB;
+using ConquiánServidor.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConquiánServidor.Services
+{
+    public class Login : ILogin
+    {
+        public bool SignIn(string playerEmail, string playerPassword)
+        {
+            try
+            {
+                using (var context = new ConquiánDBEntities())
+                {
+                    var player = context.Player.FirstOrDefault(p => p.email == playerEmail && p.password == playerPassword);
+                    return player != null;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error al iniciar sesión: " + ex.Message);
+                return false;
+            }
+        }
+    }
+}

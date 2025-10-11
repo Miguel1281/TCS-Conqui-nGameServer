277199c82f2564b47c9bd39ff60f0c74a19578d4
GabrielFdez6 <zs23014071@estudiantes.uv.mx>
2025-10-11 02:09:28 -0600

    Agrega DataContract de Player

--- TCS-ConquianGameServer/ConquiánServidor/ConquiánDB/ConquiánModelDB.Designer.cs
+++ TCS-ConquianGameServer/ConquiánServidor/ConquiánDB/ConquiánModelDB.Designer.cs
@@ -1,4 +1,4 @@
-﻿// La generación de código T4 está habilitada para el modelo 'C:\PruebaServidor\TCS-ConquianGameServer-a87a851c766a7a58ad51af42bcbfc286b9a8acaf\TCS-ConquianGameServer-a87a851c766a7a58ad51af42bcbfc286b9a8acaf\ConquiánServidor\ConquiánDB\ConquiánModelDB.edmx'. 
+﻿// La generación de código T4 está habilitada para el modelo 'C:\Users\braya\Documentos\1uv\5 semestre\Construcion\TCS-Conqui-nGameServer\TCS-ConquianGameServer\ConquiánServidor\ConquiánDB\ConquiánModelDB.edmx'. 
 // Para habilitar la generación de código heredada, cambie el valor de la propiedad del diseñador 'Estrategia de generación de código'
 // por 'ObjectContext heredado'. Esta propiedad está disponible en la ventana Propiedades cuando se abre
 // el modelo en el diseñador.
---
+++ TCS-ConquianGameServer/ConquiánServidor/DataContract/Player.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConquiánServidor.DataContract
+{
+    [DataContract]
+    public partial class Player
+    {
+        [DataMember]
+        public int idPlayer { get; set; }
+
+        [DataMember]
+        public string name { get; set; }
+
+        [DataMember]
+        public string lastName { get; set; }
+
+        [DataMember]
+        public string nickname { get; set; }
+
+        [DataMember]
+        public string email { get; set; }
+
+        [DataMember]
+        public string level { get; set; }
+
+        [DataMember]
+        public string currentPoints { get; set; }
+    }
+}
